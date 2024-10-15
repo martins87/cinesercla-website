74d1555a4f9cb681a4ee5d39ba131abfc4430100
Daniel Martins <martinsda@gmail.com>
2024-10-14 22:45:08 -0300

    fix font error

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,30 +5,25 @@ import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
 
-const helveticaNeue = localFont({
-  src: "./fonts/HelveticaNeueLTStdRoman.otf",
-  variable: "--font-helvetica-neue",
-});
-
 const geistSans = localFont({
   src: [
     {
-      path: "./fonts/Geist-Regular.otf",
+      path: "./assets/fonts/Geist-Regular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/Geist-SemiBold.otf",
+      path: "./assets/fonts/Geist-SemiBold.otf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./fonts/Geist-Bold.otf",
+      path: "./assets/fonts/Geist-Bold.otf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/Geist-ExtraBold.otf",
+      path: "./assets/fonts/Geist-ExtraBold.otf",
       weight: "800",
       style: "normal",
     },
@@ -49,7 +44,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${helveticaNeue.variable} ${geistSans.variable} antialiased bg-background text-white`}
+        className={`${geistSans.variable} antialiased bg-background text-white`}
       >
         <Navbar />
         {children}
